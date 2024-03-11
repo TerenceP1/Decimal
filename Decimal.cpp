9a539464ecbc26cec0b1f0c56c0858a122a00000
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-03-11 16:44:26 -0400

    Constructor and destructor for bitmap.

--- Decimal.cpp
+++ Decimal.cpp
@@ -1,1 +1,16 @@
 
+class bitmap {
+private:
+    unsigned long long size;
+    unsigned long long len;
+    unsigned char* bmp;
+public:
+    bitmap(unsigned long long sz){
+        len=(sz+3)/4;
+        bmp=new unsigned char[len];
+        size=sz;
+    }
+    ~bitmap(){
+        delete[] bmp;
+    }
+};
