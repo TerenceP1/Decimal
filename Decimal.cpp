46d7494d40c0946f25cacf0b29cb679de81d9a27
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-03-12 07:37:02 -0400

    Format code
    Formatted manually by Microsoft’s default clang-format.

--- Decimal.cpp
+++ Decimal.cpp
@@ -5,12 +5,12 @@ private:
     unsigned long long len;
     unsigned char* bmp;
 public:
-    bitmap(unsigned long long sz){
-        len=(sz+3)/4;
-        bmp=new unsigned char[len];
-        size=sz;
+    bitmap(unsigned long long sz) {
+        len = (sz + 3) / 4;
+        bmp = new unsigned char[len];
+        size = sz;
     }
-    ~bitmap(){
+    ~bitmap() {
         delete[] bmp;
     }
     class bitRef {
@@ -18,23 +18,24 @@ public:
         unsigned char* byte;
         short int offset;
     public:
-        bitRef(unsigned char* bt, short int os){
-            byte=bt;
-            offset=os;
+        bitRef(unsigned char* bt, short int os) {
+            byte = bt;
+            offset = os;
         }
-        operator bool() const{
-            return (byte&(1<<offset))>>offset;
+        operator bool() const {
+            return (byte & (1 << offset)) >> offset;
         }
-        void operator =(bool x){
-            if (x){
-                *byte=*byte|(1<<offset);
-            }else{
-                *byte=*byte&(~(1<<offset));
+        void operator = (bool x){
+            if (x) {
+                *byte = *byte | (1 << offset);
+            }
+            else {
+                *byte = *byte & (~(1 << offset));
             }
         }
     }
-    bitRef operator[](unsigned long long x){
-        bitRef res(bmp+(x/4),x%4);
+    bitRef operator[] (unsigned long long x) {
+        bitRef res(bmp + (x / 4), x % 4);
         return res;
     }
 };
